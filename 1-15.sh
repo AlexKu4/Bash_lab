0286cc3ecc8d53d30a3e5e653b3a6886efb98e80
Alexander Kuznetsov <71138865+AlexKu4@users.noreply.github.com>
2024-02-15 11:18:23 +0300

    Add files via upload
    First version without environmental variables

---
+++ 1-15.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+number1=1   #bottom bound
+number2=300 #upper bound
+num3=1 #counter
+if [ -n "$1" ]
+then
+	if [ $1 -ge $number1 ] && [ $1 -le $number2 ]; then #compare the number from user with bounds
+	  while [ $num3 -le $1 ] #while counter less than num from user
+	    do 
+	      echo "dir" `pwgen $num3` "$num3"; #print necessary string with password len = current couner and current counter
+	      ((num3+=1)) #increase counter
+	    done
+	exit 0; #exit with code 0 when code correct finished 
+	  
+	else  #if condition not completed 
+	  echo "Number out of range" #print message of mistake
+	exit 1 #not null exit when user gave incorrect number
+	fi;
+else
+	echo "You should give parametr when you start the program"
+exit 1	#not null exit when user don't gave number 
+fi; 
